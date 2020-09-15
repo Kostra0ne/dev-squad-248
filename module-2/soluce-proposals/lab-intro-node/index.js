cc2e48adb1167025f803fd1245c4bd8a48f6f47a
KostraOne <guillaume.amangoua@gmail.com>
2020-09-15 15:10:53 +0200

    soluce proposals for module 2

---
+++ module-2/soluce-proposals/lab-intro-node/index.js
@@ -0,0 +1,40 @@
+class SortedList {
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) throw new Error("OutOfBounds");
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return this.items[this.items.length - 1];
+  }
+
+  min() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return this.items[0];
+  }
+
+  sum() {
+    return this.items.reduce((acc, val) => acc + val, 0);
+  }
+
+  avg() {
+    if (this.items.length === 0) throw new Error("EmptySortedList");
+    return this.sum() / this.items.length;
+  }
+}
+
+module.exports = SortedList;
+
+
+
