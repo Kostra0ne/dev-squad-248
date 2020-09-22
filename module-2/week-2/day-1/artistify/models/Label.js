95df6b0925c854c79ed0fef99711314bd9209f0a
yeeeehaw <frankomarmier@gmail.com>
2020-09-22 11:45:07 +0200

    Artist and Album models

--- module-2/week-2/day-1/artistify/models/Label.js
+++ module-2/week-2/day-1/artistify/models/Label.js
@@ -4,11 +4,12 @@ const Schema = mongoose.Schema;
 const labelSchema = new Schema({
   name: { type: String },
   country: String,
+  city: String,
+  address: String,
   logo: {
     type: String,
     default: "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png",
   },
-  city: String,
 });
 
 const Label = mongoose.model("Label", labelSchema);
