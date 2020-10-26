8234fa33dedf3ce8648985f38cac88cc7c2973cf
KostraOne <guillaume.amangoua@gmail.com>
2020-10-26 13:01:08 +0100

    added DIY components (add user and user card)

---
+++ module-4/vue-intro-squad-248/src/components/UserCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <!--  here: defined a custom event named user-clicked (can be whatever name you need) -->
+  <div class="user card">
+    name: {{ user.name }} <br />
+    lastname: {{ user.lastname }} <br />
+    age: {{ user.age }} <br />
+    email: {{ user.email }}
+    <button @click="$emit('user-select', user)">select</button>
+    <button @click="$emit('user-remove', user)">remove</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  margin: 20px auto;
+  border: 1px solid;
+  padding: 10px;
+  border-radius: 0.4rem;
+  max-width: 300px;
+}
+</style>
---
+++ module-4/vue-intro-squad-248/src/components/UserFormAdd.vue
@@ -0,0 +1,22 @@
+<template>
+  <form @submit.prevent="$emit('add-new-user', user)">
+    <input type="text" placeholder="name" v-model="user.name" />
+    <input type="number" placeholder="age" v-model.number="user.age" />
+    <input type="text" placeholder="email" v-model="user.email" />
+    <button>add user</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "",
+        age: "",
+        email: ""
+      }
+    };
+  }
+};
+</script>
